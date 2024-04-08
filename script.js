4d195d40435f83acee3b8f652fa867c0e20d3fe2
Sourabh Bansotiya <77042105+sourabh-bansotiya@users.noreply.github.com>
2024-04-09 00:01:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 function validEmail(str) {
   //your JS code here.
+	const pattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+
+	return pattern.test(str);
 }
 
 // Do not change the code below.
